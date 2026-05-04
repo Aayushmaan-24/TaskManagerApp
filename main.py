70562681d9f7527f5f93e0e4564b0de444437933
Aayushmaan <aayushmaan.chakraborty@gmail.com>
2026-05-04 20:16:03 +0530

    added basic home page validation

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ from src.utils.db import Base, engine
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="TaskManager")
+
+@app.get("/")
+def home():
+    return {
+        "message": "Welcome to TaskManager API!"
+    }
