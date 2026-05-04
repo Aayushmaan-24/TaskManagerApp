e1915c9c51811d549aba6b3c66d7f813c0360dd2
Aayushmaan <aayushmaan.chakraborty@gmail.com>
2026-05-05 00:29:32 +0530

    GET endpoint created and verified

--- src/tasks/controller.py
+++ src/tasks/controller.py
@@ -1,6 +1,7 @@
 from src.tasks.dtos import TaskSchema
 from sqlalchemy.orm import Session
 from src.tasks.models import TaskModel
+from fastapi import HTTPException, status
 
 
 def create_task(body: TaskSchema, db:Session):
@@ -23,4 +24,14 @@ def get_all_tasks(db:Session):
     return {
         "status" : "Tasks fetched successfully!",
         "data" : tasks
+    }
+    
+    
+def get_task_by_id(task_id: int, db:Session):
+    task = db.query(TaskModel).get(task_id)
+    if not task:
+        return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Task not found!")
+    return {
+        "status" : "Task fetched successfully!",
+        "data" : task
     }
--- src/tasks/router.py
+++ src/tasks/router.py
@@ -14,3 +14,7 @@ def create_task(body: TaskSchema, db = Depends(get_db)):
 @task_routes.get("/all_tasks")
 def get_all_tasks(db = Depends(get_db)):
     return controller.get_all_tasks(db)
+
+@task_routes.get("/task/{task_id}")
+def get_task_by_id(task_id: int, db = Depends(get_db)):
+    return controller.get_task_by_id(task_id, db)
