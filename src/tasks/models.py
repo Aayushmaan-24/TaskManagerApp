eca3c1442fe6ba6ec7a92ce9ef80d171a7c12d00
Aayushmaan <aayushmaan.chakraborty@gmail.com>
2026-05-04 20:27:24 +0530

    basic Table structure created

---
+++ src/tasks/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, Text, Boolean
+from src.utils.db import Base
+
+class TaskModel(Base):
+    
+    __tablename__ = "user_tasks"
+    
+    id = Column(Integer, primary_key=True)
+    title = Column(String(255))
+    description = Column(Text)
+    is_completed = Column(Boolean, default=False)
