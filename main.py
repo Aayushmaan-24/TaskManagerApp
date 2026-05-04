728f54e19d33dae89311680d06a96e2907ed8deb
Aayushmaan <aayushmaan.chakraborty@gmail.com>
2026-05-04 23:00:02 +0530

    integrated POST endpoint

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI, Request, HTTPException
 from src.utils.db import Base, engine
 from src.tasks.models import TaskModel
+from src.tasks.router import task_routes
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="TaskManager")
+
+app.include_router(task_routes)
 
 @app.get("/")
 def home():
